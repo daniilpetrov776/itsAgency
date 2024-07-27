92d857684e180dee5f59ec722e7e5d13bf7d8dff
Danila Petrov <daniilpetrov776@gmail.com>
2024-07-27 15:39:30 +0300

    Сетки

---
+++ source/scripts/select.js
@@ -0,0 +1,14 @@
+const select = document.querySelector('.select');
+const selectValue = document.querySelector('.select__value');
+const selectDropdown = document.querySelector('.select__dropdown');
+console.log(selectDropdown)
+
+const onSelectDropdownClick = (evt) => {
+  const option = evt.target.closest('.select__option');
+  if (option) {
+    selectValue.textContent = option.textContent;
+    evt.target.closest('.select').blur();
+  }
+};
+
+selectDropdown.addEventListener('click', onSelectDropdownClick);
