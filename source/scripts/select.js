8a456fcec639f0bed0253ca69b8edfd78b61de4f
Danila Petrov <daniilpetrov776@gmail.com>
2024-07-28 16:20:25 +0300

    Стили всё

--- source/scripts/select.js
+++ source/scripts/select.js
@@ -1,12 +1,21 @@
 const selectValue = document.querySelector('.select__value');
-const selectDropdown = document.querySelector('.select__dropdown');
+const selectBackground = document.querySelector('.cards-header__select-background');
 
-const onSelectDropdownClick = (evt) => {
+const onSelectClick = (evt) => {
   const option = evt.target.closest('.select__option');
+
   if (option) {
     selectValue.textContent = option.textContent;
     evt.target.closest('.select').blur();
+    selectBackground.classList.add('cards-header__select-background--hidden');
+    return;
+  }
+
+  if (evt.target === selectValue) {
+    selectBackground.classList.remove('cards-header__select-background--hidden');
+  } else {
+    selectBackground.classList.add('cards-header__select-background--hidden');
   }
 };
 
-selectDropdown.addEventListener('click', onSelectDropdownClick);
+document.addEventListener('click', onSelectClick);
