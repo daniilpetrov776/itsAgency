cb1e09a44dd203e30ce75109aea79b23d107fc2c
Danila Petrov <daniilpetrov776@gmail.com>
2024-07-31 11:07:53 +0300

    Поправил адаптив

--- build/scripts/render-products.js
+++ build/scripts/render-products.js
@@ -1,1 +1,54 @@
-var i=document.querySelector("#card").content.querySelector(".products__products-list-item"),p=document.querySelector("#card-cart").content.querySelector(".cart__cart-list-item"),l=document.querySelector(".products__products-list"),x=document.querySelector(".cart__cart-list"),_=s=>{let c=document.createDocumentFragment();s.forEach(({id:m,name:r,price:n,image:e})=>{let t=i.cloneNode(!0),a=t.querySelector(".card__card-image"),d=t.querySelector(".card__card-price"),u=t.querySelector(".card__card-title"),o=t.querySelectorAll("source");o[0].srcset=`./images/mock-item${e}@1x.webp, ./images/mock-item${e}@2x.webp 2x`,o[1].srcset=`./images/mock-item${e}@1x.png, ./images/mock-item${e}@2x.png 2x`,o[2].srcset=`./images/mock-item-mobile${e}@1x.webp, ./images/mock-item-mobile${e}@2x.webp 2x`,t.dataset.id=m,a.src=`./images/mock-item-mobile${e}@1x.png`,a.srcset=`./images/mock-item-mobile${e}@2x.png`,d.textContent=`${n} \u20BD`,u.textContent=r,c.appendChild(t)}),l.appendChild(c)},g=({id:s,name:c,price:m,image:r})=>{let n=document.createDocumentFragment(),e=p.cloneNode(!0),t=e.querySelector(".cart-product__image"),a=e.querySelector(".cart-product__price"),d=e.querySelector(".cart-product__title"),u=e.querySelector("source"),o=e.querySelector(".cart-product__product-counter");u.srcset=`./images/mock-item-cart${r}@1x.webp, ./images/mock-item-cart${r}@2x.webp 2x`,e.dataset.id=s,t.src=`./images/mock-item-cart${r}@1x.png`,t.srcset=`./images/mock-item-cart${r}@2x.png`,a.textContent=`${m} \u20BD`,d.textContent=c,o.textContent=1,o.dataset.counter=s,n.appendChild(e),x.appendChild(n)},q=()=>{l.querySelectorAll(".card").forEach(c=>c.remove())};export{q as removeProducts,_ as renderProducts,g as renderToCart};
+// source/scripts/render-products.js
+var cardTemplate = document.querySelector("#card").content.querySelector(".products__products-list-item");
+var cartTemplate = document.querySelector("#card-cart").content.querySelector(".cart__cart-list-item");
+var cardsList = document.querySelector(".products__products-list");
+var cartList = document.querySelector(".cart__cart-list");
+var renderProducts = (products) => {
+  const fragment = document.createDocumentFragment();
+  products.forEach(({ id, name, price, image }) => {
+    const card = cardTemplate.cloneNode(true);
+    const cardImage = card.querySelector(".card__card-image");
+    const cardPrice = card.querySelector(".card__card-price");
+    const cardTitle = card.querySelector(".card__card-title");
+    const sources = card.querySelectorAll("source");
+    sources[0].srcset = `./images/mock-item${image}@1x.webp, ./images/mock-item${image}@2x.webp 2x`;
+    sources[1].srcset = `./images/mock-item${image}@1x.png, ./images/mock-item${image}@2x.png 2x`;
+    sources[2].srcset = `./images/mock-item-mobile${image}@1x.webp, ./images/mock-item-mobile${image}@2x.webp 2x`;
+    card.dataset.id = id;
+    cardImage.src = `./images/mock-item-mobile${image}@1x.png`;
+    cardImage.srcset = `./images/mock-item-mobile${image}@2x.png`;
+    cardPrice.textContent = `${price} \u20BD`;
+    cardTitle.textContent = name;
+    fragment.appendChild(card);
+  });
+  cardsList.appendChild(fragment);
+};
+var renderToCart = ({ id, name, price, image }) => {
+  const fragment = document.createDocumentFragment();
+  const card = cartTemplate.cloneNode(true);
+  const cardImage = card.querySelector(".cart-product__image");
+  const cardPrice = card.querySelector(".cart-product__price");
+  const cardTitle = card.querySelector(".cart-product__title");
+  const source = card.querySelector("source");
+  const counter = card.querySelector(".cart-product__product-counter");
+  source.srcset = `./images/mock-item-cart${image}@1x.webp, ./images/mock-item-cart${image}@2x.webp 2x`;
+  card.dataset.id = id;
+  cardImage.src = `./images/mock-item-cart${image}@1x.png`;
+  cardImage.srcset = `./images/mock-item-cart${image}@2x.png`;
+  cardPrice.textContent = `${price} \u20BD`;
+  cardTitle.textContent = name;
+  counter.textContent = 1;
+  counter.dataset.counter = id;
+  fragment.appendChild(card);
+  cartList.appendChild(fragment);
+};
+var removeProducts = () => {
+  const elements = cardsList.querySelectorAll(".card");
+  elements.forEach((element) => element.remove());
+};
+export {
+  removeProducts,
+  renderProducts,
+  renderToCart
+};
+//# sourceMappingURL=render-products.js.map
